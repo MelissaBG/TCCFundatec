d6c20064f5c2637c499f3a2eaf69f78efad7ff64
MelissaBG <melissabgoulart@gmail.com>
2023-05-03 21:10:12 -0300

    Commit realizado depois do intervalo aula 03-05-2023

--- src/main/java/com/fundatec/tcc/repository/TimeRepository.java
+++ src/main/java/com/fundatec/tcc/repository/TimeRepository.java
@@ -2,6 +2,8 @@ package com.fundatec.tcc.repository;
 
 import com.fundatec.tcc.model.Time;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TimeRepository extends MongoRepository<Time, String> {
 }
